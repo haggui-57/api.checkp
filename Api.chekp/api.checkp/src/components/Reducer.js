9ed0cd4f3d7b5af372da4d8a3012a9a21470a77a
haggui-57 <61077351+haggui-57@users.noreply.github.com>
2021-09-03 16:29:43 +0100

    Add files via upload

---
+++ Api.chekp/api.checkp/src/components/Reducer.js
@@ -0,0 +1,16 @@
+import { GET_CONTACTS } from "../action/actionTypes"
+
+const initState ={
+    contacts :[]
+}
+
+export const  reducer =(state=initState , action)=>{
+
+    switch (action.type) {
+
+        case GET_CONTACTS :
+        return {...state, contacts : action.payload}
+
+        default : return state
+    }
+}
